1b37c60995f6612869bd082ca07c315df3e99ea3
Chris Ashman <chrisashman96@gmail.com>
2020-10-08 15:32:33 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; /* code change */
+import shoppingListItemReducer from './reducers/shoppingListItemReducer';
+import App from './App';
+import './index.css';
 
-class App extends Component {
-
-  handleOnClick() {
-    this.props.dispatch({
-      type: 'INCREASE_COUNT',
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={(event) => this.handleOnClick()}>
-          Click
-        </button>
-        <p>{this.props.items.length}</p>
-      </div>
-    );
-  }
+const mapStateToProps = (state) => {
+  return { items: state.items };
 };
+ 
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+ 
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, /* code change */
+  document.getElementById('root')
+);
 
-export default App;
+export default connect(mapStateToProps)(App);
